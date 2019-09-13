62392d1f59a0ab02daeca11098074bcd82920f29
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-13 09:18:12 -0600

    put in boiler plate for server, inserted library dependancies.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+const app = express()
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then(databaseConnection => {
+    app.set('db', databaseConnection)
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
+})
+.catch(err => console.log(err))
